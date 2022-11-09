aecbcaebe48f13b173b2db93b05ca2d64bcdcf86
TalonFox <105325988+Talon396@users.noreply.github.com>
2022-11-08 20:03:39 -0600

    Caret autoscroll stuff
    Signed-off-by: TalonFox <105325988+Talon396@users.noreply.github.com>

--- data/Kitsune/DocumentView.lua
+++ data/Kitsune/DocumentView.lua
@@ -98,7 +98,7 @@ end
 local function ensureVisibility(docView)
     local padding = (#tostring(#docView.document.lines)*8)+8
     docView.scrollPos.dest.x = ((docView.caretPos.x-1) // ((docView.size.w-padding)//8)) * (docView.size.w-padding)
-    
+    docView.scrollPos.dest.y = ((docView.caretPos.y-1) // (docView.size.h//16)) * docView.size.h
 end
 
 function DocumentView:onKeyPress(k)
